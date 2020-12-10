d90bf89ca47bf4d78ced9e3b21debd70191fb7e3
Kristi Ingco <kristi.ingco@gmail.com>
2020-12-09 22:26:43 -0800

    Add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './components/pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home
-        imageURL="https://64.media.tumblr.com/3ea08052e7c57b0c06ccdbae8c34cdb3/1d5d9363b08e4751-6e/s640x960/2751402315c46681fc7e8810fe2dff1d7bba4ab7.jpg"
-        description="I'm Kristi Ingco and I like to code meaningful and usable products for people."  
-        navigationLinks={[{
-          "title": "Home",
-          "link": "https://google.com"
-          }
-        ]}
-        socialLinks={[{
-          "socialMedia": "twitter",
-          "link": "https://twitter/arcanekind"
-        }]}
-        />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home
+              imageURL="https://64.media.tumblr.com/3ea08052e7c57b0c06ccdbae8c34cdb3/1d5d9363b08e4751-6e/s640x960/2751402315c46681fc7e8810fe2dff1d7bba4ab7.jpg"
+              description="I'm Kristi Ingco and I like to code meaningful and usable products for people."  
+              navigationLinks={[{
+                "title": "Home",
+                "link": "https://google.com"
+                }
+              ]}
+              socialLinks={[{
+                "socialMedia": "twitter",
+                "link": "https://twitter/arcanekind"
+              }]}
+              />
+          </Route>
+        </Switch>
+        
+        
+      </div>
+    </Router>
   );
 }
 
