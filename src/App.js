9ab2c2c725e42995d2011de0a031329b86382204
Kristi Ingco <kristi.ingco@gmail.com>
2020-12-09 23:08:21 -0800

    Update navigationLinks object

--- src/App.js
+++ src/App.js
@@ -13,8 +13,20 @@ function App() {
               imageURL="https://64.media.tumblr.com/3ea08052e7c57b0c06ccdbae8c34cdb3/1d5d9363b08e4751-6e/s640x960/2751402315c46681fc7e8810fe2dff1d7bba4ab7.jpg"
               description="I'm Kristi Ingco and I like to code meaningful and usable products for people."  
               navigationLinks={[{
-                "title": "Home",
-                "link": "https://google.com"
+                  "title": "About",
+                  "link": "/about"
+                },
+                {
+                  "title": "Experience",
+                  "link": "/experience"
+                },
+                {
+                  "title": "Skills",
+                  "link": "/skills"
+                }, 
+                {
+                  "title": "Contact Me",
+                  "link": "/contact"
                 }
               ]}
               socialLinks={[{
